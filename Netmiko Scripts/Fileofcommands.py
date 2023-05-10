72fa5d5f49d8da77bfe593a8490ce2151bec3cd3
StrawberryFields0 <stararmani04@gmail.com>
2023-05-10 12:15:30 -0400

    Changes to be committed: 	new file:   Netmiko Scripts/Fileofcommands.py 	new file:   Netmiko Scripts/iosv_l2_cisco_design

---
+++ Netmiko Scripts/Fileofcommands.py
@@ -0,0 +1,31 @@
+from netmiko import ConnectHandler
+
+iosv_l2_s6 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.248',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+iosv_l2_s5 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.249',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+iosv_l2_s4 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.250',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+
+with open('ios_l2_cisco_dsign') as f:
+    lines = f.read().splitlines()
+print (lines)
+
+all_devices = [iosv_l2_s4, iosv_l2_s5, iosv_l2_s6]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print (output)
