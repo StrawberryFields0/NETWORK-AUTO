67cf62f7a169be832846f70aa7a45d17b4767993
StrawberryFields0 <stararmani04@gmail.com>
2023-05-10 05:03:55 -0400

    Changes to be committed: 	modified:   switchvlanloops.py

--- switchvlanloops.py
+++ switchvlanloops.py
@@ -18,7 +18,7 @@ tn.write(b"enable\n")
 tn.write(b"cisco\n")
 tn.write(b"conf t\n")
 for n in range (2,11):
-    tn.write(b"vlan " + str(n).encode('ascii') + b"\m")
+    tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
     tn.write(b"name Python_Vlan_" + str(n).encode('ascii') + b"\n")
 tn.write(b"end\n")
 tn.write(b"copy run start\n")
