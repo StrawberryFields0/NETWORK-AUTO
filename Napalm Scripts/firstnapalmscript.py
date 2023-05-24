1bd3c3ad582abeda2afa28f2bcb23c7044615efe
StrawberryFields0 <stararmani04@gmail.com>
2023-05-24 04:48:21 -0400

    Changes to be committed: 	modified:   Napalm Scripts/firstnapalmscript.py

--- Napalm Scripts/firstnapalmscript.py
+++ Napalm Scripts/firstnapalmscript.py
@@ -1,7 +1,14 @@
 from napalm import get_network_driver
 driver = get_network_driver('ios')
-iosvl2 = driver('192.168.122.253','cisco','cisco')
-iosvl2.open()
-
-ios_output = iosvl2.get_facts()
+iosvl2_1 = {
+        'hostname': '192.168.122.253',
+        'username': 'cisco',
+        'password': 'cisco',
+         "optional_args": {"secret": 'cisco'}
+        }
+iosvl2_1_conn = driver(**iosvl2_1)
+iosvl2_1_conn.open()
+print(f"Connecting to {iosvl2_1['hostname']}")
+ios_output = iosvl2_1_conn.get_facts()
+iosvl2_1_conn.close()
 print (ios_output)
